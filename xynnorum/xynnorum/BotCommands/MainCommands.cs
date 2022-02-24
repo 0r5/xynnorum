2a4f69b9efc1b17074d9808d54bd6b7d38ba576d
tacolift <65902496+tacolift@users.noreply.github.com>
2022-02-24 17:29:55 +0200

    Add files via upload

---
+++ xynnorum/xynnorum/BotCommands/MainCommands.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Discord.Commands;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+namespace xynnorum.BotCommands
+{
+    public class MainCommands : ModuleBase<SocketCommandContext>
+    {
+        /* ping */
+        #region Ping
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong!");
+        }
+        #endregion Ping
+        /* proxies */
+        #region Proxies
+        [Command("getproxies")]
+        public async Task GetProxies(string? ptype = null)
+        {
+            if(ptype == null)
+            {
+                await ReplyAsync("```proxy types: \nhttp\nsocks4\nsocks5```");
+                return;
+            }
+
+            if(ptype == "http")
+            {
+                WebClient client = new WebClient();
+                string webData = client.DownloadString("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=650");
+                await Context.Channel.SendMessageAsync($"```{webData.ToString()}```");
+            } else if (ptype == "socks4")
+            {
+                WebClient client = new WebClient();
+                string webData = client.DownloadString("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks4&timeout=650");
+                await Context.Channel.SendMessageAsync($"```{webData.ToString()}```");
+            } else if (ptype == "socks5")
+            {
+                WebClient client = new WebClient();
+                string webData = client.DownloadString("https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=1000");
+                await Context.Channel.SendMessageAsync($"```{webData.ToString()}```");
+            }
+        }
+        #endregion Proxies
+        /* phone lookup */
+        #region PhoneLookup
+        [Command("phonelookup")]
+        public async Task Lookup(string? num = null)
+        {
+            await Context.Channel.SendMessageAsync("PS: You need to enter it like this: ``+(COUNTRYCODE)(PHONENUMBER)``");
+            if (num == null) {
+                await ReplyAsync("You need to enter a phone number. Example: ``+(COUNTRYCODE)123456``");
+            }
+            WebClient client = new WebClient();
+
+            string plook = client.DownloadString("https://api.telnyx.com/anonymous/v2/number_lookup/" + num);
+            await Context.Channel.SendMessageAsync($"```{plook}```");
+        }
+        #endregion PhoneLookup
+        /* ip lookup */
+        #region IPLookup
+        [Command("iplookup")]
+        public async Task IPLookup(string? ip = null) {
+            if (ip == null) {
+                await ReplyAsync("You need to specify an IP.");
+            }
+            WebClient client = new WebClient();
+            string iplook = client.DownloadString($"https://ipinfo.io/{ip}/json");
+            await Context.Channel.SendMessageAsync($"```{iplook}```");
+        }
+        #endregion IPLookup
+    }
+}
---
+++ xynnorum/xynnorum/BotCommands/Moderation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord;
+
+namespace xynnorum.BotCommands
+{
+    public class Moderation : ModuleBase<SocketCommandContext>
+    {
+        /* ban */
+        #region Ban
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You don't have the permissions to ban members in this server.")]
+        public async Task Ban(IGuildUser? user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("You did not specify an user!");
+                return;
+            }
+            if (reason == null) reason = "reason was not specified";
+
+            await Context.Guild.AddBanAsync(user, 1, reason);
+            EmbedBuilder banem = new EmbedBuilder()
+                .WithDescription(":monkey:")
+                .WithTimestamp(DateTime.UtcNow)
+                .AddField($"a fag was banned", $"banned {user} for {reason}");
+            await Context.Channel.SendMessageAsync("", false, banem.Build());
+        }
+        #endregion Ban
+        /* kick */
+        #region Kick
+        [Command("kick")]
+        [RequireUserPermission(GuildPermission.KickMembers, ErrorMessage = "You don't have the permissions to ban members in this server.")]
+        public async Task Kick(IGuildUser? user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("You did not specify an user!");
+                return;
+            }
+            if (reason == null) reason = "reason was not specified";
+
+            await user.KickAsync(reason);
+            EmbedBuilder kickem = new EmbedBuilder()
+                .WithDescription(":monkey:")
+                .WithTimestamp(DateTime.UtcNow)
+                .AddField($"a fag was kicked", $"kicked {user} for {reason}");
+            await Context.Channel.SendMessageAsync("", false, kickem.Build());
+        }
+        #endregion Kick
+        /* unban */
+        #region Unban
+        [Command("unban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "You don't have the permissions to ban members in this server.")]
+        public async Task Unban(IGuildUser? user = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("You did not specify an user!");
+                return;
+            }
+
+            await Context.Guild.RemoveBanAsync(user);
+            EmbedBuilder unbanem = new EmbedBuilder()
+                .WithDescription(":monkey:")
+                .WithTimestamp(DateTime.UtcNow)
+                .AddField($"someone was unbanned", $"unbanned {user}");
+            await Context.Channel.SendMessageAsync("", false, unbanem.Build());
+        }
+        #endregion Unban
+    }
+}
